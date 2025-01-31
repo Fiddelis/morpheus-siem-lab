908de2b0b58d8e377f10f11749f1c668aea97fca
Fiddelis <lucas.ruan@ges.inatel.br>
2025-01-31 15:27:36 -0300

    Adicionado .py ao volume

--- morpheus/main.py
+++ morpheus/main.py
@@ -2,9 +2,10 @@ import logging
 from morpheus.config import Config
 from morpheus.pipeline import LinearPipeline
 from morpheus.stages.general.monitor_stage import MonitorStage
+from morpheus.utils.http_utils import HTTPMethod
 from morpheus.stages.input.http_server_source_stage import HttpServerSourceStage
-from morpheus.stages.output.write_to_elasticsearch_stage import WriteToElasticsearchStage
 from morpheus.utils.logger import configure_logging
+from morpheus.stages.output.http_client_sink_stage import HttpClientSinkStage
 
 def run_pipeline():
     # Configura o Log do Morpheus
@@ -16,23 +17,22 @@ def run_pipeline():
     pipeline = LinearPipeline(config)
 
     # Configura o estágio de entrada como um servidor HTTP
-    pipeline.set_source(HttpServerSourceStage(config))
+    pipeline.set_source(HttpServerSourceStage(config,
+                            bind_address="0.0.0.0",
+                            port=80,
+                            lines=True,
+                            endpoint="/message"))
 
     # Adiciona um estágio de monitoramento
     pipeline.add_stage(MonitorStage(config))
 
     # Configura o estágio de escrita para o Elasticsearch
-    elasticsearch_stage = WriteToElasticsearchStage(
-        config=config,
-        index="morpheus-logs",
-        host="http://localhost:9200",
-        auth=("elastic", "teste@123")
-    )
-
-    # Adiciona o estágio ao pipeline
-    pipeline.add_stage(elasticsearch_stage)
-
-    # Executa o pipeline
+    pipeline.add_stage(HttpClientSinkStage(config,
+                            base_url="http://elastic:123456@elasticsearch:9200",
+                            endpoint="/suricata-log/_doc",
+                            method=HTTPMethod.POST,
+                            lines=True,
+                            headers={"Content-Type": "application/json"}))
     pipeline.run()
 
 if __name__ == "__main__":
